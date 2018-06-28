7c7c3a6951d4068a506495c45c3f5dfad295a43d
Arthur B <arthur@berzieri.fr>
2018-06-28 19:51:56 +0200

    [MRUSSIA] Added md5 encryption for a string

--- src/main/java/fr/arthb/motherrussia/utils/AppUtils.java
+++ src/main/java/fr/arthb/motherrussia/utils/AppUtils.java
@@ -9,9 +9,9 @@ import org.springframework.http.HttpStatus;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
 import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -40,6 +40,34 @@ public class AppUtils {
             cookie.setPath("/");
             response.addCookie(cookie);
         }
+    }
+
+    public static String md5(String str) {
+        byte[] uniqueKey = str.getBytes();
+        byte[] hash      = null;
+
+        try
+        {
+            hash = MessageDigest.getInstance("MD5").digest(uniqueKey);
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            throw new Error("No MD5 support in this VM.");
+        }
+
+        StringBuilder hashString = new StringBuilder();
+        for (int i = 0; i < hash.length; i++)
+        {
+            String hex = Integer.toHexString(hash[i]);
+            if (hex.length() == 1)
+            {
+                hashString.append('0');
+                hashString.append(hex.charAt(hex.length() - 1));
+            }
+            else
+                hashString.append(hex.substring(hex.length() - 2));
+        }
+        return hashString.toString();
     }
 
     public static String getPassHashed(String pass){
