a611c03d0fc69adf90c833c025872d2355653457
Arthur B <arthur@berzieri.fr>
2018-06-28 15:26:39 +0200

    [MRUSSIA] gain calcualator logs + fixed error contacting API

--- src/main/java/fr/arthb/motherrussia/controller/GainCalulatorController.java
+++ src/main/java/fr/arthb/motherrussia/controller/GainCalulatorController.java
@@ -3,6 +3,8 @@ import fr.arthb.motherrussia.constants.AppConstants;
 import fr.arthb.motherrussia.exception.CustomException;
 import fr.arthb.motherrussia.service.AuthenticationService;
 import fr.arthb.motherrussia.service.GainCalculatorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -19,9 +21,14 @@ public class GainCalulatorController {
 	@Autowired
 	private GainCalculatorService gainCalculatorService;
 
+    private static Logger logger = LoggerFactory.getLogger(GainCalulatorController.class);
+
+    private final String LOG_PREFIX = "getGainCalculator() -> ";
+
 	@GetMapping(path = "{gameId}", consumes = "application/json", produces = "application/json;charset=UTF-8")
 	public void getGainCalculator(HttpServletRequest request, @PathVariable Integer gameId) {
 
+        logger.info(LOG_PREFIX + "gameId: " + gameId);
         int gamblerId = authenticationService.getGamblerId(request);
         // If gambler's id found
         if(
@@ -30,8 +37,10 @@ public class GainCalulatorController {
                 gamblerId == 13 || // Antho
                 gamblerId == 20    // Krimo
         ) {
+            logger.info(LOG_PREFIX + "Authorized gambler: " + gamblerId);
             gainCalculatorService.process(gameId);
         } else {
+            logger.error(LOG_PREFIX + "Unauthorized gambler: " + gamblerId);
             throw new CustomException(AppConstants.TRICHE, HttpStatus.FORBIDDEN);
         }
 
--- src/main/java/fr/arthb/motherrussia/service/impl/GainCalculatorServiceImpl.java
+++ src/main/java/fr/arthb/motherrussia/service/impl/GainCalculatorServiceImpl.java
@@ -17,6 +17,9 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.Set;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -37,12 +40,16 @@ public class GainCalculatorServiceImpl implements GainCalculatorService {
     @Autowired
     private PronosticRepository pronosticRepository;
 
+    private static Logger logger = LoggerFactory.getLogger(GainCalculatorService.class);
+
     private final String USER_AGENT = "Mozilla/5.0";
+
+    private final String LOG_PREFIX = "process() -> ";
 
     public void process(int gameId) {
 
         String url = "http://api.football-data.org/v1/fixtures/" + String.valueOf(gameId);
-
+        logger.info(LOG_PREFIX + "Call endpoint: " + url);
         URL obj = null;
         try {
             obj = new URL(url);
@@ -50,6 +57,7 @@ public class GainCalculatorServiceImpl implements GainCalculatorService {
             e.printStackTrace();
         }
 
+        boolean responseOK = true;
         StringBuffer response = new StringBuffer();
         try {
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
@@ -58,60 +66,100 @@ public class GainCalculatorServiceImpl implements GainCalculatorService {
             con.setRequestProperty("User-Agent", USER_AGENT);
 
             int responseCode = con.getResponseCode();
-            System.out.println("\nSending 'GET' request to URL : " + url);
-            System.out.println("Response Code : " + responseCode);
+            if(con.getResponseCode() != 200) {
+                logger.error(LOG_PREFIX + "ResponseCode: "+ con.getResponseCode());
+                responseOK = false;
+            } else {
+                logger.info(LOG_PREFIX + "ResponseCode: "+ con.getResponseCode());
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine;
 
-
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
             }
-            in.close();
         } catch (IOException e) {
+            responseOK = false;
             e.printStackTrace();
         }
 
-        JsonObject responseJson = new JsonParser().parse(response.toString()).getAsJsonObject();
-        JsonObject fixtureResults = responseJson.getAsJsonObject("fixture").getAsJsonObject("result");
+        if(responseOK == false) {
+            logger.error(LOG_PREFIX + "Aborted process");
+        } else {
+            logger.info(LOG_PREFIX + "Response: " + response.toString());
+            JsonObject responseJson = new JsonParser().parse(response.toString()).getAsJsonObject();
+            JsonObject fixtureResults = responseJson.getAsJsonObject("fixture").getAsJsonObject("result");
 
-        int fixtureResultsHome = fixtureResults.get("goalsHomeTeam").getAsInt();
-        int fixtureResultsAway = fixtureResults.get("goalsAwayTeam").getAsInt();
+            int fixtureResultsHome = fixtureResults.get("goalsHomeTeam").getAsInt();
+            int fixtureResultsAway = fixtureResults.get("goalsAwayTeam").getAsInt();
 
-        Set<Pronostic> pronostics = gameService.getOne(gameId).getPronostics();
-        for (Pronostic pronostic : pronostics) {
-            int gain = 0;
+            logger.info(LOG_PREFIX + "gameService.getOne(" + String.valueOf(gameId) + ").getPronostics()");
+            Set<Pronostic> pronostics = gameService.getOne(gameId).getPronostics();
 
-            // OK winning team
-            if(AppUtils.getWiningTeam(pronostic.getProno1(), pronostic.getProno2()) == AppUtils.getWiningTeam(fixtureResultsHome, fixtureResultsAway)) {
-                gain ++;
+            if(pronostics == null) {
+                logger.error(LOG_PREFIX + "No pronostics for game: " + String.valueOf(gameId) + " found");
+            } else {
+                for (Pronostic pronostic : pronostics) {
+                    int gain = 0;
 
-                // OK 1 team
-                if(
-                        (pronostic.getProno1() == fixtureResultsHome && pronostic.getProno2() != fixtureResultsAway) ||
-                                (pronostic.getProno1() != fixtureResultsHome && pronostic.getProno2() == fixtureResultsAway)
-                ) {
-                    gain ++;
+                    // OK winning team
+                    if(AppUtils.getWiningTeam(pronostic.getProno1(), pronostic.getProno2()) == AppUtils.getWiningTeam(fixtureResultsHome, fixtureResultsAway)) {
+                        gain ++;
+
+                        // OK 1 team
+                        if(
+                                (pronostic.getProno1() == fixtureResultsHome && pronostic.getProno2() != fixtureResultsAway) ||
+                                        (pronostic.getProno1() != fixtureResultsHome && pronostic.getProno2() == fixtureResultsAway)
+                        ) {
+                            gain ++;
+                        }
+                    }
+
+                    // OK all pronostics
+                    if(pronostic.getProno1() == fixtureResultsHome && pronostic.getProno2() == fixtureResultsAway) {
+                        gain = 4;
+                    }
+
+                    logger.info(
+                        LOG_PREFIX +
+                        "Pronostic: " + String.valueOf(pronostic.getId()) +
+                        "; gamblerId: " + String.valueOf(pronostic.getGambler().getId()) +
+                        "; pronos: " +
+                        String.valueOf(pronostic.getProno1()) + "-" + String.valueOf(pronostic.getProno2()) +
+                        "; scores: " +
+                        String.valueOf(fixtureResultsHome) + "-" + String.valueOf(fixtureResultsAway) +
+                        "; gain: " + String.valueOf(gain)
+                    );
+                    pronostic.setGain(gain);
+                    pronosticRepository.save(pronostic);
                 }
+                logger.info(LOG_PREFIX + "Ended saving pronostics");
             }
 
-            // OK all pronostics
-            if(pronostic.getProno1() == fixtureResultsHome && pronostic.getProno2() == fixtureResultsAway) {
-                gain = 4;
-            }
+            logger.info(LOG_PREFIX + "Find all gamblers");
+            Iterable<Gambler> gamblers = gamblerService.findAll();
 
-            pronostic.setGain(gain);
-            pronosticRepository.save(pronostic);
+            logger.info(LOG_PREFIX + "Found "+ Long.toString(gamblers.spliterator().getExactSizeIfKnown()) + " gamblers");
+            for (Gambler gambler : gamblers) {
+                int gain = gamblerRepository.getGainSum(gambler.getId());
+                logger.info(
+                    LOG_PREFIX +
+                    "Gambler: " + String.valueOf(gambler.getId()) +
+                    "; previousGain: " + String.valueOf(gambler.getGain()) +
+                    "; newGain: " + String.valueOf(gain)
+                );
+                gambler.setGain(gain);
+                gamblerRepository.save(gambler);
+            }
+            logger.info(LOG_PREFIX + "Ended saving gamblers");
         }
 
 
-        Iterable<Gambler> gamblers = gamblerService.findAll();
-        for (Gambler gambler : gamblers) {
-            int gain = gamblerRepository.getGainSum(gambler.getId());
-            gambler.setGain(gain);
-            gamblerRepository.save(gambler);
-        }
+
+
+
 
         // param userId - int gainSum = gamblerRepository.getGainSum();
     }
